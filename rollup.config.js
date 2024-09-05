6a2a2cf44ecd56730687b7a89551f5065cdf2e63
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2024-09-06 00:46:57 +0700

    fix: optimize rollup build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,49 @@
 import typescript from "rollup-plugin-typescript2";
 import terser from "@rollup/plugin-terser";
+import { dts } from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "dist/index.js",
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-      strict: false,
-    },
-  ],
-  plugins: [
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
-    terser({
-      format: {
-        comments: false,
+const config = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: false,
       },
-    }),
-  ],
-};
+    ],
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+      }),
+      terser({
+        format: {
+          comments: false,
+        },
+      }),
+    ],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+  {
+    input: "dist/index.js",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
+        strict: false,
+      },
+    ],
+    plugins: [del({ targets: "dist/types" })],
+  },
+];
+
+export default config;
